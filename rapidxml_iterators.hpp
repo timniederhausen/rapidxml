0f6c8a1212ac78e229b962f9960744946e0b265b
Tim Niederhausen <tim@rnc-ag.de>
2016-03-07 00:07:16 +0100

    Extend iterator features and add iterator ranges

--- rapidxml_iterators.hpp
+++ rapidxml_iterators.hpp
@@ -10,31 +10,33 @@
 
 namespace rapidxml
 {
+    const unsigned int iterate_check_name = 1 << 0;
+    const unsigned int iterate_case_sensitive = 1 << 1;
 
     //! Iterator of child nodes of xml_node
-    template<class Ch>
+    template <class Ch = char>
     class node_iterator
     {
-
     public:
-
-        typedef typename xml_node<Ch> value_type;
-        typedef typename xml_node<Ch> &reference;
-        typedef typename xml_node<Ch> *pointer;
+        typedef xml_node<Ch> value_type;
+        typedef xml_node<Ch> &reference;
+        typedef xml_node<Ch> *pointer;
         typedef std::ptrdiff_t difference_type;
         typedef std::bidirectional_iterator_tag iterator_category;
 
         node_iterator()
             : m_node(0)
+            , m_flags(0)
         {
         }
 
-        node_iterator(xml_node<Ch> *node)
-            : m_node(node->first_node())
+        node_iterator(xml_node<Ch>* node, unsigned char flags)
+            : m_node(node)
+            , m_flags(flags)
         {
         }
 
-        reference operator *() const
+        reference operator*() const
         {
             assert(m_node);
             return *m_node;
@@ -49,7 +51,12 @@ namespace rapidxml
         node_iterator& operator++()
         {
             assert(m_node);
-            m_node = m_node->next_sibling();
+            if (m_flags & iterate_check_name)
+                m_node = m_node->next_sibling(m_node->name(),
+                                              m_node->name_size(),
+                                              !!(m_flags & iterate_case_sensitive));
+            else
+                m_node = m_node->next_sibling();
             return *this;
         }
 
@@ -62,8 +69,14 @@ namespace rapidxml
 
         node_iterator& operator--()
         {
-            assert(m_node && m_node->previous_sibling());
-            m_node = m_node->previous_sibling();
+            assert(m_node);
+            if (m_flags & iterate_check_name)
+                m_node = m_node->previous_sibling(m_node->name(),
+                                                  m_node->name_size(),
+                                                  !!(m_flags & iterate_case_sensitive));
+            else
+                m_node = m_node->previous_sibling();
+            assert(m_node);
             return *this;
         }
 
@@ -74,46 +87,45 @@ namespace rapidxml
             return tmp;
         }
 
-        bool operator ==(const node_iterator<Ch> &rhs)
+        bool operator==(const node_iterator<Ch> &rhs)
         {
             return m_node == rhs.m_node;
         }
 
-        bool operator !=(const node_iterator<Ch> &rhs)
+        bool operator!=(const node_iterator<Ch> &rhs)
         {
             return m_node != rhs.m_node;
         }
 
     private:
-
         xml_node<Ch> *m_node;
-
+        unsigned char m_flags;
     };
 
     //! Iterator of child attributes of xml_node
-    template<class Ch>
+    template <class Ch = char>
     class attribute_iterator
     {
-
     public:
-
-        typedef typename xml_attribute<Ch> value_type;
-        typedef typename xml_attribute<Ch> &reference;
-        typedef typename xml_attribute<Ch> *pointer;
+        typedef xml_attribute<Ch> value_type;
+        typedef xml_attribute<Ch> &reference;
+        typedef xml_attribute<Ch> *pointer;
         typedef std::ptrdiff_t difference_type;
         typedef std::bidirectional_iterator_tag iterator_category;
 
         attribute_iterator()
             : m_attribute(0)
+            , m_flags(0)
         {
         }
 
-        attribute_iterator(xml_node<Ch> *node)
-            : m_attribute(node->first_attribute())
+        attribute_iterator(xml_attribute<Ch>* attr, unsigned char flags)
+            : m_attribute(attr)
+            , m_flags(flags)
         {
         }
 
-        reference operator *() const
+        reference operator*() const
         {
             assert(m_attribute);
             return *m_attribute;
@@ -128,7 +140,12 @@ namespace rapidxml
         attribute_iterator& operator++()
         {
             assert(m_attribute);
-            m_attribute = m_attribute->next_attribute();
+            if (m_flags & iterate_check_name)
+                m_attribute = m_attribute->next_attribute(m_attribute->name(),
+                                                          m_attribute->name_size(),
+                                                          !!(m_flags & iterate_case_sensitive));
+            else
+                m_attribute = m_attribute->next_attribute();
             return *this;
         }
 
@@ -141,8 +158,14 @@ namespace rapidxml
 
         attribute_iterator& operator--()
         {
-            assert(m_attribute && m_attribute->previous_attribute());
-            m_attribute = m_attribute->previous_attribute();
+            assert(m_attribute);
+            if (m_flags & iterate_check_name)
+                m_attribute = m_attribute->previous_attribute(m_attribute->name(),
+                                                              m_attribute->name_size(),
+                                                              !!(m_flags & iterate_case_sensitive));
+            else
+                m_attribute = m_attribute->previous_attribute();
+            assert(m_attribute);
             return *this;
         }
 
@@ -153,22 +176,78 @@ namespace rapidxml
             return tmp;
         }
 
-        bool operator ==(const attribute_iterator<Ch> &rhs)
+        bool operator==(const attribute_iterator<Ch> &rhs)
         {
             return m_attribute == rhs.m_attribute;
         }
 
-        bool operator !=(const attribute_iterator<Ch> &rhs)
+        bool operator!=(const attribute_iterator<Ch> &rhs)
         {
             return m_attribute != rhs.m_attribute;
         }
 
     private:
-
-        xml_attribute<Ch> *m_attribute;
-
+        xml_attribute<Ch>* m_attribute;
+        unsigned char m_flags;
     };
 
+    // Range-based for loop support
+    template <typename Iterator>
+    class iterator_range
+    {
+    public:
+        typedef Iterator const_iterator;
+        typedef Iterator iterator;
+
+        iterator_range(Iterator first, Iterator last)
+            : m_first(first)
+            , m_last(last)
+        {
+        }
+
+        Iterator begin() const { return m_first; }
+        Iterator end() const { return m_last; }
+
+    private:
+        Iterator m_first;
+        Iterator m_last;
+    };
+
+    template <class Ch>
+    iterator_range<node_iterator<Ch>> nodes(const xml_node<Ch>* node,
+                                            const Ch* name = 0,
+                                            std::size_t name_size = 0,
+                                            bool case_sensitive = true)
+    {
+        unsigned char flags = 0;
+        if (name)
+            flags |= iterate_check_name;
+        if (case_sensitive)
+            flags |= iterate_case_sensitive;
+
+        node_iterator<Ch> first(node->first_node(name, name_size, case_sensitive),
+                                flags);
+        node_iterator<Ch> last(0, flags);
+        return iterator_range<node_iterator<Ch>>(first, last);
+    }
+
+    template <class Ch>
+    iterator_range<attribute_iterator<Ch>> attributes(const xml_node<Ch>* node,
+                                                      const Ch *name = 0,
+                                                      std::size_t name_size = 0,
+                                                      bool case_sensitive = true)
+    {
+        unsigned char flags = 0;
+        if (name)
+            flags |= iterate_check_name;
+        if (case_sensitive)
+            flags |= iterate_case_sensitive;
+
+        attribute_iterator<Ch> first(node->first_attribute(name, name_size, case_sensitive),
+                                     flags);
+        attribute_iterator<Ch> last(0, flags);
+        return iterator_range<attribute_iterator<Ch>>(first, last);
+    }
 }
 
 #endif
---
+++ tests/test_iterators/main.cpp
@@ -0,0 +1,62 @@
+#include "../test_utils.hpp"
+#include "../../rapidxml.hpp"
+#include "../../rapidxml_utils.hpp"
+#include "../../rapidxml_iterators.hpp"
+#include <string>
+#include <stdexcept>
+#include <iterator>
+#include <algorithm>
+#include <iostream>
+
+using namespace rapidxml;
+using namespace std;
+
+template<int Flags>
+string name(xml_base<char> *node)
+{
+    if (Flags & parse_no_string_terminators)
+        return string(node->name(), node->name() + node->name_size());
+    else
+        return string(node->name());
+}
+
+template<int Flags>
+void test_all()
+{
+    file<> f("../xml_files/simple_all.xml");
+
+    rapidxml::xml_document<char> doc;
+    doc.parse<Flags>(f.data());
+
+    CHECK(node_iterator<>() == node_iterator<>());
+
+    iterator_range<node_iterator<>> n1 = nodes(&doc, "root");
+    CHECK(1 == std::distance(n1.begin(), n1.end()));
+
+    iterator_range<node_iterator<>> n2 = nodes(&doc, "invalid");
+    CHECK(0 == std::distance(n2.begin(), n2.end()));
+
+    xml_node<>* root = doc.first_node("root");
+    CHECK(root);
+
+    iterator_range<attribute_iterator<>> n3 = attributes(root);
+    CHECK(1 == std::distance(n1.begin(), n1.end()));
+    CHECK(name<Flags>(&*n3.begin()) == "entities");
+}
+
+int main()
+{
+    try {
+        test_all<parse_fastest>();
+        test_all<parse_non_destructive>();
+        test_all<0>();
+        test_all<parse_trim_whitespace>();
+        test_all<parse_normalize_whitespace>();
+        test_all<parse_trim_whitespace | parse_normalize_whitespace>();
+        test_all<parse_full>();
+    } catch (std::exception& e) {
+        std::cout << e.what() << "\n";
+    }
+
+    return test::final_results();
+}
